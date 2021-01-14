1b8f945d5112539c99a565c92fa7e104eb5d80e7
Ragi-lys <yuusan1694301@gmail.com>
2021-01-14 12:09:21 +0900

    Init

--- pages/bibiography/index.php
+++ pages/bibiography/index.php
@@ -8,9 +8,9 @@
   ?>
 </head>
 <?php
-$dbsn = 'mysql:dbname=tb220963db;host=localhost';
-$user = "tb-220963";
-$password ="rwd7r23gHn";
+$dbsn = '**DATABASE**';
+$user = "**USER**";
+$password = "**PASS**";
 
 $pdo = new PDO($dbsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 
@@ -35,11 +35,11 @@ $stmt = $pdo->query($init_sql);
 
 <body>
   <?php
-function Reg_comment($pdo=null, $mission=null, $tag=null, $url=null, $description=null)
-{
+  function Reg_comment($pdo = null, $mission = null, $tag = null, $url = null, $description = null)
+  {
     if ($pdo == null) {
-        echo("Database is null");
-        return null;
+      echo ("Database is null");
+      return null;
     }
     /* データの挿入． */
     $post_sql =   $pdo->prepare("INSERT INTO Bibliography (mission, tag, url, comment) VALUES (:mission, :tag, :url, :comment)");
@@ -50,53 +50,53 @@ function Reg_comment($pdo=null, $mission=null, $tag=null, $url=null, $descriptio
     $post_sql->bindParam(':comment', $description, PDO::PARAM_STR);
     /* 登録に成功すると，trueを返す． */
     if ($post_sql->execute()) {
-        return true;
+      return true;
     } else {
-        return false;
+      return false;
     }
-}
+  }
 
-function Load_DB(PDO $pdo=null)
-{
+  function Load_DB(PDO $pdo = null)
+  {
     if ($pdo != null) {
-        $split_sql = "DELETE FROM Bibliography WHERE mission IS NULL";
-        $stmt = $pdo->prepare($split_sql);
-        $stmt->execute();
+      $split_sql = "DELETE FROM Bibliography WHERE mission IS NULL";
+      $stmt = $pdo->prepare($split_sql);
+      $stmt->execute();
 
-        $match_sql = "SELECT * FROM Bibliography";
-        $stmt = $pdo->prepare($match_sql);
-        if ($stmt->execute()) { /* もし成功したら */
-            $results = $stmt->fetchAll();   /* SQL文で出力した結果を格納． */
-            $amount =  count($results);   /* 1であれば抽出成功． */
-            if ($amount == 0) {
-                echo "<tr><td colspan='3'>登録データがありません。</td></tr>";
-                return null;
-            }
-            $content = array();
-            foreach ($results as $row) {    //* 抽出結果について，出力． */
-                $mission = $row['mission'];
-                $tag = $row['tag'];
-                $comment = $row['comment'];
-                $url = $row['url'];
-                $tuple =
-                "<tr>"
-                ."<td>". $mission ."</td>"
-                ."<td> <a style='width:100%; height:100%; display:block;' href='".$url."'>".$tag."</td>"
-                . "<td>". $comment ."</td>"
-                ."</tr>";
-                array_push($content, $tuple);
-            }
-            return $content;
-        } else {
-            var_dump($pdo->errorInfo());
-            return null;
+      $match_sql = "SELECT * FROM Bibliography";
+      $stmt = $pdo->prepare($match_sql);
+      if ($stmt->execute()) { /* もし成功したら */
+        $results = $stmt->fetchAll();   /* SQL文で出力した結果を格納． */
+        $amount =  count($results);   /* 1であれば抽出成功． */
+        if ($amount == 0) {
+          echo "<tr><td colspan='3'>登録データがありません。</td></tr>";
+          return null;
         }
+        $content = array();
+        foreach ($results as $row) {    //* 抽出結果について，出力． */
+          $mission = $row['mission'];
+          $tag = $row['tag'];
+          $comment = $row['comment'];
+          $url = $row['url'];
+          $tuple =
+            "<tr>"
+            . "<td>" . $mission . "</td>"
+            . "<td> <a style='width:100%; height:100%; display:block;' href='" . $url . "'>" . $tag . "</td>"
+            . "<td>" . $comment . "</td>"
+            . "</tr>";
+          array_push($content, $tuple);
+        }
+        return $content;
+      } else {
+        var_dump($pdo->errorInfo());
+        return null;
+      }
     }
-}
+  }
 
 
 
-?>
+  ?>
 
 
 
@@ -120,9 +120,9 @@ function Load_DB(PDO $pdo=null)
           <?php
           $content = Load_DB($pdo);
           if ($content != null) {
-              foreach ($content as $item) {
-                  echo $item;
-              }
+            foreach ($content as $item) {
+              echo $item;
+            }
           }
           ?>
         </tbody>
@@ -141,28 +141,36 @@ function Load_DB(PDO $pdo=null)
           <select id="mission-select" class="custom-select text-center" name="mission_correct">
             <option value="-1" disabled selected>▽ 選択</option>
             <?php
-                                    $mission_all = [
-                                        ["Mission 3",
-                                            "3-1","3-2","3-3","3-4","3-5 （★）"],
-                                        ["Mission 4",
-                                            "4-1","4-2","4-3","4-4","4-5","4-6","4-7","4-8"],
-                                        ["Mission 5",
-                                            "5-1  （★）","5-2"],
-                                        ["Mission 6",
-                                            "6"]
-                                        ];
-                                        foreach ($mission_all as $item) {
-                                            for ($i = 0; $i < count($item); $i++) {
-                                                if ($i == 0) {
-                                                    echo "<option value=".$item[0]
-                                                ." disabled>◆".$item[0]."</option>";
-                                                } else {
-                                                    echo "<option value=".explode(" ", $item[$i])[0]
-                                                .">".$item[$i]."</option>";
-                                                }
-                                            }
-                                        }
-?>
+            $mission_all = [
+              [
+                "Mission 3",
+                "3-1", "3-2", "3-3", "3-4", "3-5 （★）"
+              ],
+              [
+                "Mission 4",
+                "4-1", "4-2", "4-3", "4-4", "4-5", "4-6", "4-7", "4-8"
+              ],
+              [
+                "Mission 5",
+                "5-1  （★）", "5-2"
+              ],
+              [
+                "Mission 6",
+                "6"
+              ]
+            ];
+            foreach ($mission_all as $item) {
+              for ($i = 0; $i < count($item); $i++) {
+                if ($i == 0) {
+                  echo "<option value=" . $item[0]
+                    . " disabled>◆" . $item[0] . "</option>";
+                } else {
+                  echo "<option value=" . explode(" ", $item[$i])[0]
+                    . ">" . $item[$i] . "</option>";
+                }
+              }
+            }
+            ?>
           </select>
 
         </div>
@@ -178,11 +186,9 @@ function Load_DB(PDO $pdo=null)
 
         <div class="form-group">
           <label for="inputDescribe">説明文</label>
-          <textarea id="inputDescribe" class="form-control" rows="3" name="inputDescribe"
-            placeholder="どのような時に参照したかを書いてくれるとありがたいです！" required></textarea>
+          <textarea id="inputDescribe" class="form-control" rows="3" name="inputDescribe" placeholder="どのような時に参照したかを書いてくれるとありがたいです！" required></textarea>
         </div>
-        <button id="register" name="register" class="btn btn-primary btn-block" type="submit" data-toggle="modal"
-          data-target="#confirm">登録</button>
+        <button id="register" name="register" class="btn btn-primary btn-block" type="submit" data-toggle="modal" data-target="#confirm">登録</button>
       </form>
 
     </div>
@@ -190,30 +196,30 @@ function Load_DB(PDO $pdo=null)
 
   <?php
   $is_Register = null;
-   if (isset($_POST["register"])) {
-       $is_Register = Reg_comment(
-           $pdo,
-           $_POST["mission_correct"],
-           $_POST["inputLabel"],
-           $_POST["inputURL"],
-           $_POST["inputDescribe"]
-       );
-   } else {
-       $_POST = array();
-   }
-   if ($is_Register == false) {
-       exit;
-   }
+  if (isset($_POST["register"])) {
+    $is_Register = Reg_comment(
+      $pdo,
+      $_POST["mission_correct"],
+      $_POST["inputLabel"],
+      $_POST["inputURL"],
+      $_POST["inputDescribe"]
+    );
+  } else {
+    $_POST = array();
+  }
+  if ($is_Register == false) {
+    exit;
+  }
   ?>
   <script>
-  setInterval(function() {
-    location.reload();
-  }, 30 * 1000);
+    setInterval(function() {
+      location.reload();
+    }, 30 * 1000);
 
-  ///////////////////////////////////////
-  $('#register').on('click', function() {
-    location.reload();
-  });
+    ///////////////////////////////////////
+    $('#register').on('click', function() {
+      location.reload();
+    });
   </script>
 
 
