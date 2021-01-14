f10c50c7c46f2c7bcc189ba4b2ceee2f2b6ea95d
Ragi-lys <yuusan1694301@gmail.com>
2021-01-14 12:05:21 +0900

    Init

--- index.php
+++ index.php
@@ -2,157 +2,95 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <!-- jQuery and JS bundle w/ Popper.js -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
-    </script>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet">
-
-    <title>Aチーム専用ページ</title>
+  <?php
+    $title = "Aチーム専用ポータルサイト";
+    include_once('component/menu.php');
+    include_once('component/method/line.php');
+    $_SESSION["CLIENT_VERSION"] = isset($_SESSION["CLIENT_VERSION"]) ?  $_SESSION["CLIENT_VERSION"] : $_ENV["VERSION"];
+    ?>
+  <title><?php echo $title?>
+  </title>
 </head>
 
+<body class="container-fluid">
 
 
-<body>
-    <?php
-    function getConfig(string $file_config)
-    {
-        if (file_exists($file_config)) {
-            $conf = file_get_contents($file_config);
-            $arr = json_decode($conf, true);
-            return $arr;
-        } else {
-            return null;
-        }
-    }
-    date_default_timezone_set('Asia/Tokyo');
-    $config = getConfig("package.json");
-    $get_up = strtotime($config['update']);
-    $update  = date("Y/m/d", $get_up);
-    ?>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <a class="navbar-brand">Aチーム専用ページ</a>
-        <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div id="my-nav" class="collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">TOP<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#!" data-target="#askpass" data-toggle="modal">チーム資料<span
-                            class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#iken">意見箱<span class="sr-only">(current)</span></a>
-                </li>
-                <!-- 内部情報(Version,Updated dateなど) -->
-                <li class="divider my-2"></li>
-                <li class="nav-item text-right">
-                    <a class="nav-link  disabled " href="#" aria-disabled="true">Version: <?php echo $config["version"];?></a>
-                    <a class="nav-link  disabled " href="#" aria-disabled="true">Updated: <?php echo $update;?></a>
-
-                </li>
-                <!--  <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Item 2</a>
-                </li> -->
-            </ul>
+  <div class="">
+    <h4 style="text-align: center;">
+      <?php echo $title?>
+    </h4>
+    <hr>
+    <div class="container-fluid" id="update_history">
+      <div class="d-flex justify-content-between">
+        <h5>更新履歴</h5>
+        <div><?php echo "　（最終更新日：".date('Y/m/d', $update)."）";  ?>
         </div>
-    </nav>
+      </div>
+      <li>『チームの進捗状況』を廃止し，ミーティングに気軽に参加できるようにしました．</li>
+      <li> <strong>意見箱</strong> で，送信したときに空白のページに移動する不具合を修正しました．</li>
 
+    </div>
+    <hr>
+  </div>
+  <div>
+    <div name="today" class="text-center">
 
-    <!-- 本文 -->
-    <div class="container-fluid">
-        <div class="my-4 ">
-            <h3 style="text-align: center;">
-                Aチームのページへようこそ！</h3>
-            <p>作成途中での公開です．<br>
-                もしほしい機能があれば，メニューの意見箱からどんどん投稿してくださいね！</p>
-            <hr>
+    </div>
+    <div class="text-center">
+      <div class="card border-dark">
+        <div class="card-header ">
+          実装予定の機能
         </div>
-        <div>
-            <div class="card border-dark">
-                <div class="card-header text-center">
-                    事務連絡
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item active"></li>
-                    <li class="list-group-item disabled" aria-disabled="true"></li>
-                </ul>
-            </div>
-            <div class="card my-3 border-info">
-                <div class="card-header text-center">
-                    チーム進捗状況
-                </div>
-                <div class="card-body text-center">
-                    <div class="navbar-text">工事中</div>
-                </div>
-            </div>
+        <ul class="list-group ">
+          <li class="list-group-item ">各地の天気表示</li>
+        </ul>
+      </div>
+      <div class="card my-3 border-info">
+        <div class="card-header">
+          チームミーティングに参加する
         </div>
+        <div class="card-body">
+          <p>参加方法を選択してください。</p>
+          <div class="d-flex justify-content-center">
+            <a class=" btn btn-primary w-50 mx-2" role="button" href=<?php echo $_ENV['ZOOMURL_APP']; ?>>アプリで参加</a>
+            <a class=" btn btn-warning w-50 mx-2" role="button" href=<?php echo $_ENV['ZOOMURL_BR'] ;?>>ブラウザで参加</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
-        <hr>
-        <div id="nippou">
+  <hr>
+  <!--   <div id="nippou">
             <h2>日報作成</h2>
 
 
             <hr>
-        </div>
-        <div id="iken">
-            <h2>意見箱</h2>
+        </div> -->
+  <div id="iken" class="container-fluid">
+    <h5>意見箱</h5>
 
-            <hr>
-        </div>
+    <form action="" method="post" class="m-4" name="suggest" id="sug">
+      <div class="form-group">
+        <p>こういう機能が欲しいとかあれば，ぜひ！</p>
+        <textarea id="suggestion" class="form-control" placeholder="送信された内容は，リーダとサブリーダ間で共有されます。" name="suggestion"
+          rows="3"></textarea>
+      </div>
+      <span id="sug_res"></span>
+      <div class="d-flex justify-content-between">
+        <button class="btn btn-secondary w-50 mx-2" type="reset">リセット</button>
+        <button class="btn btn-primary w-50 mx-2" type="submit" id="sug_post">送信</button>
+      </div>
+      <div class="response"><?php echo $receive_message; ?>
+    </form>
+  </div>
 
 
-    </div>
 
-    <!-- ダイアログ系 -->
-    <div id="askpass" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="askpass-t" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="askpass-t">外部サイトへの遷移</h5>
-                </div>
-                <div class="modal-body text-center">
-                    <p>チーム資料フォルダに移動しますか？
-                        (外部サイトに移動します)</p>
-                </div>
-                <div class="modal-footer w-90">
-                    <a href="https://drive.google.com/drive/folders/1a2ukYaUSSjP1k3WxaDP7VSRFqH7UjdMd"
-                        class="btn btn-primary btn-block col" role="button">OK</a>
 
-                    <button class="btn btn-danger btn-block col" data-dismiss="modal" aria-label="Close">
-                        <span>キャンセル</span>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
 
 
 </body>
 
-
-
-<style>
-    body {
-        /* padding: 50px 0;
-         */
-        font-family: 'Noto Sans JP', sans-serif;
-    }
-</style>
 
 </html>
